bf7a8b3b366ccf3e581b1e224c686387f3dda5b2
EhsanKINUX <ehsankhwentao@gmail.com>
2024-05-09 16:47:17 +0330

    feat: create root main page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSibar from "@/components/RightSibar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "EKinux", lastName: "KH", email: "ekinux@gamil.com" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1234.87} />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSibar user={loggedIn} transactions={[]} banks={[{currentBalance: 234.87}, {currentBalance: 221.65}]} />
+    </section>
+  );
+};
+
+export default Home;
